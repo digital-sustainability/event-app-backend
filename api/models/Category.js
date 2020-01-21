6faac532d7b845670fe1b74f10509b04e21c2213
Roland Widmer <roland.widmer@inf.unibe.ch>
2020-01-21 13:59:24 +0100

    Category, EventCategory models added
    refs #2582 @1.5h

--- api/models/Category.js
+++ api/models/Category.js
@@ -1,5 +1,5 @@
 /**
- * Event.js
+ * Category.js
  *
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
@@ -8,51 +8,15 @@
 module.exports = {
 
   attributes: {
+
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-    title: {
+    name: {
       type: 'string',
       required: true
     },
-    description: {
-      type: 'string',
-      required: true
-    },
-    start: {
-      type: 'ref',
-      required: true,
-      columnType: 'DATETIME'
-    },
-    end: {
-      type: 'ref',
-      required: true,
-      columnType: 'DATETIME'
-    },
-    location: {
-      type: 'string',
-      required: true
-    },
-    location_details: {
-      type: 'string',
-      allowNull:true
-    },
-    image_path: {
-      type: 'string',
-      allowNull: true
-    },
-    url: {
-      type: 'string',
-      allowNull: true
-    },
-    url_label: {
-      type: 'string',
-      allowNull: true
-    },
-    published: {
-      type: 'boolean',
-      defaultsTo: false
-    },
+
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
@@ -62,16 +26,12 @@ module.exports = {
     //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
     //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
     //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-    sessions: {
-      collection: 'session',
-      via: 'event_id'
-    },
-    categories: {
-      collection: 'category',
-      via: 'event_id',
-      through: 'eventCategory'
-    }
 
+    events: {
+      collection: 'event',
+      via: 'category_id',
+      through: 'eventCategory'
+    },
   },
 
 };
--- api/models/EventCategory.js
+++ api/models/EventCategory.js
@@ -1,5 +1,5 @@
 /**
- * Event.js
+ * EventCategory.js
  *
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
@@ -8,51 +8,11 @@
 module.exports = {
 
   attributes: {
+
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-    title: {
-      type: 'string',
-      required: true
-    },
-    description: {
-      type: 'string',
-      required: true
-    },
-    start: {
-      type: 'ref',
-      required: true,
-      columnType: 'DATETIME'
-    },
-    end: {
-      type: 'ref',
-      required: true,
-      columnType: 'DATETIME'
-    },
-    location: {
-      type: 'string',
-      required: true
-    },
-    location_details: {
-      type: 'string',
-      allowNull:true
-    },
-    image_path: {
-      type: 'string',
-      allowNull: true
-    },
-    url: {
-      type: 'string',
-      allowNull: true
-    },
-    url_label: {
-      type: 'string',
-      allowNull: true
-    },
-    published: {
-      type: 'boolean',
-      defaultsTo: false
-    },
+
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
@@ -62,16 +22,15 @@ module.exports = {
     //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
     //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
     //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-    sessions: {
-      collection: 'session',
-      via: 'event_id'
+    event_id: {
+      model: 'event',
+      required: true
     },
-    categories: {
-      collection: 'category',
-      via: 'event_id',
-      through: 'eventCategory'
-    }
 
+    category_id: {
+      model: 'category',
+      required: true
+    }
   },
 
 };
