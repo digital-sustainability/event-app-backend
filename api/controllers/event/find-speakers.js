a14bf0c63c307a9992b0a441b72506f1ed4476d5
Roland <roland.widmer@inf.unibe.ch>
2020-10-09 12:07:16 +0200

    - event/:id/speaker and speaker/:id/populated requests adapted to support presentations of events
    refs #3589 @1.5h

--- api/controllers/event/find-speakers.js
+++ api/controllers/event/find-speakers.js
@@ -24,12 +24,17 @@ module.exports = {
 
     const query = `SELECT distinct speaker.id, first_name, last_name, email, speaker.position, organization, short_bio,
      photo_url FROM speaker, presentationspeaker, presentation, session, event WHERE
-      speaker.id = presentationspeaker.speaker_id AND
+      (speaker.id = presentationspeaker.speaker_id AND
       presentationspeaker.presentation_id = presentation.id AND
       presentation.session_id = session.id AND
       session.event_id = event.id AND
-      event.id = $1 ORDER BY first_name ASC;
-      `;
+      event.id = $1) OR
+      (speaker.id = presentationspeaker.speaker_id AND
+      presentationspeaker.presentation_id = presentation.id AND
+      presentation.event_id = event.id AND
+      event.id = $1)
+      ORDER BY first_name ASC;
+      `; // first part: speakers of presenations of sessions of the event, second part: speakers of presentations of the event
     const dataStore = Event.getDatastore();
     const speakers = await dataStore.sendNativeQuery(query, [inputs.id]);
 
